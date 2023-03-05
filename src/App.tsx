c3a3a1746b88ed97f58f77817c52e054ed610334
DJ <damianjaroszek90@gmail.com>
2023-03-05 20:17:23 +0100

    Added types configuration between frontend and backend

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-      <h1 className="underline text-3xl border-8">
-          Hello world!
-      </h1>
-  );
+    // const foobar: TestInterface = {
+    //     x: 123,
+    // };
+    return (
+        <h1 className="underline text-3xl border-8">
+            Hello world!
+        </h1>
+    );
 }
 
 export default App;
