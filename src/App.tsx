1d572d53a28b2686efd5c196168cf15d66d103dc
DJ <damianjaroszek90@gmail.com>
2023-03-06 20:09:28 +0100

    Added mock components and pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {FiSettings} from 'react-icons/fi';
 import {TooltipComponent} from "@syncfusion/ej2-react-popups";
+import {Navbar} from "./components/Navbar";
+import {Sidebar} from "./components/Sidebar";
 
 import './App.css';
+import {Ecommerce} from "./pages/Ecommerce";
+import {Orders} from "./pages/Orders";
+import {Employees} from "./pages/Employees";
+import {Customers} from "./pages/Customers";
+import {Line} from "./components/Charts/Line";
+import {Pie} from "./components/Charts/Pie";
+
 
 function App() {
     // const foobar: TestInterface = {
@@ -28,36 +37,35 @@ function App() {
                 </div>
                 {activeMenu ? (
                     <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-                        Sidebar
+                        <Sidebar/>
                     </div>
                 ) : (
                     <div className="w-0 dark:bg-secondary-dark-bg">
-                        Sidebar
+                        <Sidebar/>
                     </div>
                 )}
                 <div className={
                     `dark:bg:main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
                 }>
                     <div className="fixed md: static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-                        Navbar
+                        <Navbar/>
                     </div>
                 </div>
 
                 <div>
                     <Routes>
                         {/* Dashboard */}
-                        <Route path="/" element="ECommerce"/>
-                        <Route path="/ecommerce" element="ECommerce"/>
+                        <Route path="/" element={<Ecommerce/>}/>
+                        <Route path="/ecommerce" element={<Ecommerce/>}/>
 
                         {/* Pages */}
-                        <Route path="/orders" element="Orders"/>
-                        <Route path="/employees" element="Employees"/>
-                        <Route path="/customers" element="Customers"/>
+                        <Route path="/orders" element={<Orders/>}/>
+                        <Route path="/employees" element={<Employees/>}/>
+                        <Route path="/customers" element={<Customers/>}/>
 
                         {/* Charts */}
-                        <Route path="/line" element="Line"/>
-                        <Route path="/pie" element="Pie"/>
-                        <Route path="/customers" element="Customers"/>
+                        <Route path="/line" element={<Line/>}/>
+                        <Route path="/pie" element={<Pie/>}/>
                     </Routes>
                 </div>
 
