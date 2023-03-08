eebfe9901513c1602124c47dbc98d1746a720c26
DJ <damianjaroszek90@gmail.com>
2023-03-08 19:26:21 +0100

    Added documentation for Sidebar component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import {FiSettings} from 'react-icons/fi';
-import {TooltipComponent} from "@syncfusion/ej2-react-popups";
 import {Navbar} from "./components/Navbar";
 import {Sidebar} from "./components/Sidebar";
-
+//import {TestInterface} from 'types';
 import './App.css';
 import {Budget} from "./pages/Budget";
 import {History} from "./pages/History";
 import {Receipt} from "./pages/Receipt";
-import {useDispatch, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import {RootState} from "./store";
 
 
@@ -19,44 +17,33 @@ export const App = () => {
     // };
     //const activeMenu = true;
 
-    const dispatch = useDispatch();
-    const {activeMenu} = useSelector((store: RootState) => store.frontendComponentsState)
+
+    const {activeMenu} = useSelector((store: RootState) => store.frontendComponentsState);
 
     return (
 
-        <BrowserRouter>
-            <div className="flex relative dark:bg-main-dark-bg">
-                <div className="fixed right-4 bottom-4" style={{zIndex: '1000'}}>
-                    <TooltipComponent content="Settings" position="TopCenter">
-                        <button type="button"
-                                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
-                                style={{
-                                    background: 'blue',
-                                    borderRadius: '50%'
-                                }}>
-                            <FiSettings/>
-                        </button>
-                    </TooltipComponent>
-                </div>
+        <BrowserRouter> {/* added Router*/}
+            <div className="flex relative dark:bg-main-dark-bg"> {/* div works as container of app*/}
                 {activeMenu ? (
                     <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-                        <Sidebar/>
+                        <Sidebar/> {/*if activeMenu is true then show a column on the left side with included a Sidebar component */}
                     </div>
                 ) : (
-                    <div className="w-0 dark:bg-secondary-dark-bg">
+                    <div
+                        className="w-0 dark:bg-secondary-dark-bg"> {/*if activeMenu is false then hide a column on the left side with included a Sidebar component */}
                         <Sidebar/>
                     </div>
                 )}
                 <div className={
                     `dark:bg:main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
-                }>
+                }> {/*if activeMenu is true then set margin left 72 (18 rem) otherwise set width of element to flex 2 */}
                     <div className="fixed md: static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-                        <Navbar/>
+                        <Navbar/>{/*added Navbar component*/}
                     </div>
                 </div>
 
                 <div>
-                    <Routes>
+                    <Routes> {/*added Routes for elements of Sidebar*/}
                         {/* Dashboard */}
                         <Route path="/" element={<Budget/>}/>
                         <Route path="/budget" element={<Budget/>}/>
