1d183d1cf434b2baa7e677977208041b2b5dd34f
DJ <damianjaroszek90@gmail.com>
2023-03-23 18:16:24 +0100

    Renamed from fetch-data to fetch-data-get.tsx

--- src/utils/fetch-data-get.tsx
+++ src/utils/fetch-data-get.tsx
@@ -1,4 +1,4 @@
-export const fetchData = async (hostAddress: string, url: string) => {
+export const fetchDataGet = async (hostAddress: string, url: string) => {
     const res = await fetch(`${hostAddress}${url}`);
     const data = await res.json();
     return data;
