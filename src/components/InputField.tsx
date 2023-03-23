69352eba196dabf6343442b5b6749f2eede7d615
DJ <damianjaroszek90@gmail.com>
2023-03-23 18:23:56 +0100

    InputAutocompleteField.tsx - fixed interface Props - added handleUpdateForm - added ro TextField onChange, required, InputProps for set decimal format (is not working...)

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import {TextField} from "@mui/material";
 
 interface Props {
     fieldName: string;
     type: "number" | "string";
     width: number;
+    updateForm: (key: string, value: string | number) => void;
+    propertyNameOfObjToSet: string;
 }
 
-export const InputField = ({fieldName, type, width}: Props) => {
+export const InputField = ({fieldName, type, width, updateForm, propertyNameOfObjToSet}: Props) => {
+
+    const handleUpdateForm = (e: ChangeEvent<HTMLInputElement>) => {
+        updateForm(propertyNameOfObjToSet, Number(e.target.value));
+    }
+
     return (
         <React.Fragment>
             <label>
@@ -15,7 +22,11 @@ export const InputField = ({fieldName, type, width}: Props) => {
                 <div className="flex">
                     <TextField
                         type={type}
-                        sx={{width}}/>
+                        sx={{width}}
+                        onChange={handleUpdateForm}
+                        InputProps={{inputProps: {min: 0.00, max: 40.00}}}
+                        required
+                    />
                 </div>
             </label>
         </React.Fragment>
