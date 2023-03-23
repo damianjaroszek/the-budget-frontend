66ddc048d35971acad37ab7d082b4cbb47049c22
DJ <damianjaroszek90@gmail.com>
2023-03-23 18:27:59 +0100

    InputAutocompleteField.tsx - fixed interface Props (updateForm function and propertyNameOfObjToSet) - added props - added useEffect for update form

--- src/components/InputAutocompleteField.tsx
+++ src/components/InputAutocompleteField.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
+import {useEffect} from 'react';
 import {Autocomplete, Stack, TextField} from "@mui/material";
+
 
 interface Props {
     fieldName: string;
     width: number;
     data: any;
+    updateForm: (key: string, value: string | number) => void;
+    propertyNameOfObjToSet: string;
 }
 
-export const InputAutocompleteField = ({fieldName, width, data}: Props) => {
+export const InputAutocompleteField = ({fieldName, width, data, updateForm, propertyNameOfObjToSet}: Props) => {
 
     const defaultProps = {
         options: data,
@@ -15,30 +19,40 @@ export const InputAutocompleteField = ({fieldName, width, data}: Props) => {
     };
     const [value, setValue] = React.useState<any | null>(null);
 
+    useEffect(() => {
+        value && console.log('I see changes in value: ' + value.id);
+        value && updateForm(propertyNameOfObjToSet, value.id);
+    }, [value]);
+
 
     return (
+        <>{value && console.log(value.id)}
 
-        <Stack spacing={1}>
-            {/*<div>{`${value?.name}`}</div>*/}
-            <label>
-                <span className="text-gray-400">{fieldName}: </span>
-                <div className="flex">
-                    <Autocomplete
-                        sx={{width}}
-                        {...defaultProps}
-                        id="controlled-demo"
-                        value={value}
-                        onChange={(event: any, newValue: any | null) => {
-                            setValue(newValue);
-                        }}
-                        renderInput={(params) => (
-                            <TextField {...params} />
-                        )}
-                    />
-                </div>
-            </label>
-        </Stack>
+            <Stack spacing={1}>
+                {/*<div>{`${value?.name}`}</div>*/}
+                <label>
+                    <span className="text-gray-400">{fieldName}: </span>
+                    <div className="flex">
+                        <Autocomplete
+                            sx={{width}}
+                            {...defaultProps}
+                            id="controlled-demo"
+                            value={value}
 
+                            onChange={(event: any, newValue: any | null) => {
+                                setValue(newValue);
+                            }}
+                            // onChange={(e: SyntheticEvent) => {
+                            //     updateForm('Product', (e.target as HTMLInputElement).value)
+                            // }}
+                            renderInput={(params) => (
+                                <TextField {...params} required/>
+                            )}
+                        />
+                    </div>
+                </label>
+            </Stack>
+        </>
     );
 
 }
