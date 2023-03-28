21ca17d95cdeacd9f5669730319e065007d5af16
DJ <damianjaroszek90@gmail.com>
2023-03-28 21:22:54 +0200

    Remove mock and replace it components with loaded data from db (table) input select a date range

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,99 +1,73 @@
-import React from 'react';
-
-
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
+import React, {useEffect, useState} from 'react';
 import {Header} from "../components/Header";
+import {TableOutput} from "../components/TableOutput";
+import {fetchData} from "../utils/fetch-data";
+import {constHostAddress} from "../utils/global-const";
+import {DeleteButtonForTable} from "../components/DeleteButtonForTable";
+import {RecipeEntityWithAction} from "./Receipt";
+import {NewRecipe, ProductEntity, ShopEntity} from 'types';
+import {InputDateForm} from "../components/InputDateForm";
 
-// MATERIAL-UI
-interface Column {
-    id: 'name' | 'code' | 'population' | 'size' | 'density';
-    label: string;
-    minWidth?: number;
-    align?: 'right';
-    format?: (value: number) => string;
-}
-
-const columns: readonly Column[] = [
-    {id: 'name', label: 'Name', minWidth: 170},
-    {id: 'code', label: 'ISO\u00a0Code', minWidth: 100},
-    {
-        id: 'population',
-        label: 'Population',
-        minWidth: 170,
-        align: 'right',
-        format: (value: number) => value.toLocaleString('en-US'),
-    },
-    {
-        id: 'size',
-        label: 'Size\u00a0(km\u00b2)',
-        minWidth: 170,
-        align: 'right',
-        format: (value: number) => value.toLocaleString('en-US'),
-    },
-    {
-        id: 'density',
-        label: 'Density',
-        minWidth: 170,
-        align: 'right',
-        format: (value: number) => value.toFixed(2),
-    },
-];
-
-interface Data {
-    name: string;
-    code: string;
-    population: number;
-    size: number;
-    density: number;
-}
-
-function createData(
-    name: string,
-    code: string,
-    population: number,
-    size: number,
-): Data {
-    const density = population / size;
-    return {name, code, population, size, density};
-}
-
-const rows = [
-    createData('India', 'IN', 1324171354, 3287263),
-    createData('China', 'CN', 1403500365, 9596961),
-    createData('Italy', 'IT', 60483973, 301340),
-    createData('United States', 'US', 327167434, 9833520),
-    createData('Canada', 'CA', 37602103, 9984670),
-    createData('Australia', 'AU', 25475400, 7692024),
-    createData('Germany', 'DE', 83019200, 357578),
-    createData('Ireland', 'IE', 4857000, 70273),
-    createData('Mexico', 'MX', 126577691, 1972550),
-    createData('Japan', 'JP', 126317000, 377973),
-    createData('France', 'FR', 67022000, 640679),
-    createData('United Kingdom', 'GB', 67545757, 242495),
-    createData('Russia', 'RU', 146793744, 17098246),
-    createData('Nigeria', 'NG', 200962417, 923768),
-    createData('Brazil', 'BR', 210147125, 8515767),
-];
 
 export const History = () => {
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
-    const handleChangePage = (event: unknown, newPage: number) => {
-        setPage(newPage);
-    };
+    const recipeInitValues = {
+        date: null,
+        price: 0,
+        productId: '',
+        shopId: '',
+    }
 
-    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
-    };
+    const [recipesFromDb, setRecipesFromDb] = useState<RecipeEntityWithAction[] | null>(null);
+    const [productsFromDb, setProductsFromDb] = useState<ProductEntity[] | null>(null);
+    const [shopsFromDb, setShopsFromDb] = useState<ShopEntity[] | null>(null);
+    const [isDataSet, setIsDataSet] = useState<boolean>(false);
+    const [newRecipeFromForm, setNewRecipeFromForm] = useState<NewRecipe>(recipeInitValues);
+
+
+    const removeRecipeFromDb = async (id: string) => {
+        const recipe = await fetchData(constHostAddress, '/recipe', id, {method: 'DELETE'});
+        if (recipe[0].affectedRows === 1) {
+            setIsDataSet(prevState => !prevState);
+        }
+    }
+
+    useEffect(() => {
+
+        setRecipesFromDb(null);
+
+        const getRecipesFromDb = async () => {
+            const recipes = await fetchData(constHostAddress, '/recipe/listLatestWeek');
+
+            recipes.map((obj: RecipeEntityWithAction) => {
+                return obj.action = <DeleteButtonForTable id={obj.id} removeRecipeFromDb={removeRecipeFromDb}/>
+            })
+            setRecipesFromDb(recipes);
+
+            console.log({recipes});
+            console.log(recipesFromDb);
+            console.log(JSON.stringify(recipes) === JSON.stringify(recipesFromDb));
+
+        }
+        if (isDataSet) {
+            getRecipesFromDb().catch(console.error);
+            setIsDataSet(false);
+        }
+
+    }, [isDataSet]); //isDataSet
+
+    useEffect(() => {
+        setProductsFromDb(null);
+
+        const getProductsFromDb = async () => {
+            const products = await fetchData(constHostAddress, '/product/listAll');
+            setProductsFromDb(products);
+        };
+
+        getProductsFromDb().catch(console.error);
+        setIsDataSet(true);
+    }, []); //isDataSet
+
 
     return (
         <div className="mt-20 md:pr-10 md:pl-10 pr-3 pl-3">
@@ -120,55 +94,23 @@ export const History = () => {
                 </form>
             </div>
 
+            <div className="mt-1 mb-10 ">
+                <InputDateForm/>
+            </div>
+            <div className="mt-1 ">
+                {recipesFromDb ? <TableOutput rows={recipesFromDb}
+                                              columns={
+                                                  [{id: 'name', label: 'name', minWidth: 80},
+                                                      {id: 'date', label: 'date', minWidth: 20},
+                                                      {id: 'price', label: 'price [PLN]', minWidth: 10},
+                                                      {id: 'shopName', label: 'shop', minWidth: 10},
+                                                      {id: 'categoryName', label: 'category', minWidth: 10},
+                                                      {id: 'action', label: 'action', minWidth: 10},]
+                                              }
+                /> : <span>Loading...</span>}
 
-            <Paper sx={{width: '100%', overflow: 'hidden'}}>
-                <TableContainer sx={{maxHeight: 600}}>
-                    <Table stickyHeader aria-label="sticky table">
-                        <TableHead>
-                            <TableRow>
-                                {columns.map((column) => (
-                                    <TableCell
-                                        key={column.id}
-                                        align={column.align}
-                                        style={{minWidth: column.minWidth}}
-                                    >
-                                        {column.label}
-                                    </TableCell>
-                                ))}
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {rows
-                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                .map((row) => {
-                                    return (
-                                        <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                                            {columns.map((column) => {
-                                                const value = row[column.id];
-                                                return (
-                                                    <TableCell key={column.id} align={column.align}>
-                                                        {column.format && typeof value === 'number'
-                                                            ? column.format(value)
-                                                            : value}
-                                                    </TableCell>
-                                                );
-                                            })}
-                                        </TableRow>
-                                    );
-                                })}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <TablePagination
-                    rowsPerPageOptions={[10, 25, 100]}
-                    component="div"
-                    count={rows.length}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onPageChange={handleChangePage}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-                />
-            </Paper>
+            </div>
         </div>
+
     );
 }
