bd59e9d8927c1d7655ee30a8a0a3883c94ca10a1
DJ <damianjaroszek90@gmail.com>
2023-03-29 21:00:13 +0200

    Refactoring of InputDateForm.tsx - added DateRange type, props interface

--- src/components/InputDateForm.tsx
+++ src/components/InputDateForm.tsx
@@ -1,7 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Button as ButtonComponent} from "./Button";
 import {DatePickerInput} from "./DatePickerInput";
 
+type DateRange = {
+    firstDate: Date;
+    secondDate: Date;
+}
 
 // interface Props {
 //     productsFromDb: ProductEntity[];
@@ -10,31 +14,28 @@ import {DatePickerInput} from "./DatePickerInput";
 //     saveRecipeToDb: (e: SyntheticEvent) => Promise<void>;
 // }
 
-export const InputDateForm = (/*{updateForm, saveRecipeToDb}: Props*/) => {
+interface Props {
+    dateRange: DateRange;
+    updateForm: (key: string, value: string | number | Date | null) => void;
+    onClickFunction: any;
+}
 
-    type DateRange = {
-        firstDate: Date;
-        secondDate: Date;
-    }
+export const InputDateForm = (/*{updateForm, saveRecipeToDb}: Props*/ {
+    dateRange,
+    updateForm,
+    onClickFunction
+}: Props) => {
 
-    const [dateRange, setDateRange] = useState<DateRange>({
-        firstDate: new Date,
-        secondDate: new Date,
-    })
 
-    const updateForm = (key: string, value: string | number | Date | null) => {
-        setDateRange(prevDateRange => ({
-            ...prevDateRange,
-            [key]: value,
-        }));
-        //@todo domyślnie zapytanie z historią z dzisiaj
-    };
+    //@todo domyślnie zapytanie z historią z dzisiaj
+
     return (
         <>{console.log(dateRange)}
             <form /*onSubmit={saveRecipeToDb}*/>
-                <span className="text-gray-400">Select a date range:</span>
+                <div className="pb-3">
+                    <span className="text-gray-400">Select a date range:</span>
+                </div>
                 <div className="flex justify-start">
-
                     <label>
                         <DatePickerInput updateForm={updateForm} propertyNameOfObjToSet="firstDate"/>
                     </label>
@@ -44,7 +45,9 @@ export const InputDateForm = (/*{updateForm, saveRecipeToDb}: Props*/) => {
                     </label>
                     <div className="h-1">
                         <ButtonComponent color={"white"} bgColor={"blue"} text={"Search"} borderRadius={"5px"}
-                                         type="submit"/>
+                                         paddingLeft={3} paddingRight={3}
+                                         type="submit"
+                                         onClickFunction={onClickFunction}/>
                     </div>
                 </div>
             </form>
