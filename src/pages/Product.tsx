359d2b444c53f1f97fa77673f32603f069883141
DJ <damianjaroszek90@gmail.com>
2023-03-31 22:44:35 +0200

    Added new components - shop and product

---
+++ src/pages/Product.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Header} from "../components/Header";
+
+export const Product = () => {
+    return (
+        <>
+            <div className="mt-20 md:pr-10 md:pl-10 pr-3 pl-3">
+                <Header category={'product'} title={'product'}/>
+            </div>
+        </>
+
+    );
+};
