df13ed6491a4c654388e31ccf502868fb3a7c878
DJ <damianjaroszek90@gmail.com>
2023-04-01 21:33:42 +0200

    Deleted unused code

--- src/components/OutputListItem.tsx
+++ src/components/OutputListItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {DeleteButton} from "./DeleteButton";
+import {TbTrashOff} from "react-icons/tb";
 
 
 interface Props {
@@ -10,15 +11,9 @@ interface Props {
 }
 
 export const OutputListItem = ({name, id, removeItem, isDeletable}: Props) => {
-    //
-    // const personLiClicked = () => {
-    //     props.onRemoveItem(props.user.username);
-    // }
-
 
     return (<>
-            {/*{console.log(isAbleToDelete)}*/}
-            <li className="pb-3 sm:pb-4" key={id} /*onClick={personLiClicked}*/>
+            <li className="pb-3 sm:pb-4" key={id}>
 
                 <div className="flex items-center space-x-4">
                     <div className="flex-1 min-w-0">
@@ -28,7 +23,7 @@ export const OutputListItem = ({name, id, removeItem, isDeletable}: Props) => {
                     </div>
                     <div
                         className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white pt-5">
-                        {isDeletable === 1 && <DeleteButton id={id} removeItem={removeItem}/>}
+                        {isDeletable === 1 ? <DeleteButton id={id} removeItem={removeItem}/> : <TbTrashOff/>}
                     </div>
                 </div>
             </li>
