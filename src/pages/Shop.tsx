3d1950200cf82485a6cd7d537c6d94c495c2a0d3
DJ <damianjaroszek90@gmail.com>
2023-04-02 21:32:58 +0200

    Added properly working adding new shop to db

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,28 +1,64 @@
-import React, {useEffect, useState} from 'react';
-import {ShopEntity} from 'types';
+import React, {SyntheticEvent, useEffect, useState} from 'react';
+import {NewShopEntity, ShopEntity} from 'types';
 import {Header} from "../components/Header";
 import {fetchData} from "../utils/fetch-data";
 import {constHostAddress} from "../utils/global-const";
 import {OutputList} from "../components/OutputList";
+import {InputFormProduct} from "../components/InputFormProduct";
 
 
 export const Shop = () => {
+
+    const shopInitValues = {
+        name: '',
+    }
     const [shopsFromDb, setShopsFromDb] = useState<ShopEntity[] | null>(null);
     const [isDataSet, setIsDataSet] = useState<boolean>(false);
+    const [newShopFromForm, setNewShopFromForm] = useState<NewShopEntity>(shopInitValues);
+    const [nameExist, setNameExist] = useState<boolean>(false);
+
+    const isNameExist = () => {
+        return shopsFromDb?.map(shop => shop.name).includes(newShopFromForm.name);
+    };
+
+    const saveShopToDb = async (e: SyntheticEvent) => {
+        e.preventDefault();
+        if (!isNameExist()) {
+            const data = await fetchData(constHostAddress, '/shop', '', {
+                method: 'POST',
+                body: JSON.stringify(
+                    newShopFromForm
+                ),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            setIsDataSet(prevState => !prevState);
+        } else {
+            setNameExist(true);
+        }
+    }
+
+    const updateForm = (key: string, value: string) => {
+        setNewShopFromForm(newShopFromForm => ({
+            ...newShopFromForm,
+            [key]: value,
+        }));
+        setNameExist(false);
+
+
+    };
 
     const removeShopFromDb = async (id: string) => {
-
-
         const shop = await fetchData(constHostAddress, '/shop', id, {method: 'DELETE'}); //@todo zrobić endpoint do usuwania sklepu
         //if (shop[0].affectedRows === 1) {
         setIsDataSet(prevState => !prevState);
         //}
-
     }
 
     useEffect(() => {
         setShopsFromDb(null);
-
         const getShopsFromDb = async () => {
             const shops = await fetchData(constHostAddress, '/shop/listAll');
             setShopsFromDb(shops);
@@ -33,23 +69,22 @@ export const Shop = () => {
     }, [isDataSet]);
 
     return (
-        <>{console.log(shopsFromDb)}
+        <>{console.log(newShopFromForm)}
             <div className="mt-20 md:pr-10 md:pl-10 pr-3 pl-3">
-                <Header category={'shop'} title={'shop'}/>
+                <Header category={'page'} title={'Shop'}/>
 
                 <div className="mt-1 mb-10 ">
-                    <div className="w-80 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6">
+                    <div className="w-96 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6">
                         {shopsFromDb &&
                             <OutputList data={shopsFromDb} sortParameter={'name'} removeItem={removeShopFromDb}/>}
-
-
-                        {/*{*/}
-                        {/*    productsFromDb && shopsFromDb &&*/}
-                        {/*    <InputForm productsFromDb={productsFromDb} shopsFromDb={shopsFromDb} updateForm={updateForm}*/}
-                        {/*               saveRecipeToDb={saveRecipeToDb}/>*/}
-                        {/*}*/}
-
                     </div>
+                </div>
+            </div>
+
+            <div className="md:pr-10 md:pl-10 pr-3 pl-3">
+                <div className="w-96 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6">
+                    <InputFormProduct updateForm={updateForm} saveShopToDb={saveShopToDb}/>
+                    {nameExist && <p className="text-rose-600">The shop is already exist in database.</p>}
                 </div>
             </div>
         </>
