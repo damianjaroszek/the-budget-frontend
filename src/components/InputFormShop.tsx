96b6ce38eb1324e6e9de8eb8906d634ff1d00e8e
DJ <damianjaroszek90@gmail.com>
2023-04-02 21:33:35 +0200

    Added InputFormShop component

---
+++ src/components/InputFormShop.tsx
@@ -0,0 +1,34 @@
+import React, {SyntheticEvent} from 'react';
+import {InputField} from "./InputField";
+import {Button as ButtonComponent} from "./Button";
+
+
+interface Props {
+
+    updateForm: (key: string, value: string) => void;
+    saveShopToDb: (e: SyntheticEvent) => Promise<void>;
+}
+
+export const InputFormShop = ({updateForm, saveShopToDb}: Props) => {
+    return (
+        <>
+            <form onSubmit={saveShopToDb}>
+                <div className="flex justify-start pt-5 pr-3">
+
+
+                    <div className="pr-3 pb-3">
+                        <InputField fieldName="Add new shop" width={275} type="text" updateForm={updateForm}
+                                    propertyNameOfObjToSet="name" required={true}/>
+                    </div>
+
+
+                    <div className="pt-3 self-center">
+                        <ButtonComponent color="white" bgColor="blue" text="Add" borderRadius="1px"
+                                         type="submit" paddingRight={3} paddingLeft={3} paddingBottom={3}
+                                         paddingTop={3}/>
+                    </div>
+                </div>
+            </form>
+        </>
+    );
+};
