c25925e21894a3bff9bcc868754b305c7e882778
DJ <damianjaroszek90@gmail.com>
2023-04-03 20:09:19 +0200

    Added categories from fetch, fixed padding right for input elements of form

--- src/components/InputFormProduct.tsx
+++ src/components/InputFormProduct.tsx
@@ -1,35 +1,34 @@
 import React, {SyntheticEvent} from 'react';
 import {InputField} from "./InputField";
 import {Button as ButtonComponent} from "./Button";
+import {CategoryEntity} from 'types';
+import {InputAutocompleteField} from "./InputAutocompleteField";
 
 
 interface Props {
 
     updateForm: (key: string, value: string) => void;
     saveShopToDb: (e: SyntheticEvent) => Promise<void>;
+    categoriesFromDb: CategoryEntity[] | null;
 }
 
-const data = [{categoryId: 'categoryExapme', name: 'Jakas kategoria1'}, {
-    categoryId: 'categoryExapme2',
-    name: 'Jakas kategoria2'
-}];
-//@todo zaciągać kategorie z bazy przygotować category record i końcówkę do wyświetlania kategorii, póki co kategoria jest wpisana na sztywno do initProduct
-
-export const InputFormProduct = ({updateForm, saveShopToDb}: Props) => {
+export const InputFormProduct = ({updateForm, saveShopToDb, categoriesFromDb}: Props) => {
     return (
         <>
             <form onSubmit={saveShopToDb}>
-                <div className="flex flex-wrap justify-start pt-5 pr-3">
+                <div className="flex justify-start pt-3 pr-3">
 
 
-                    <div className="pr-3 pb-3">
+                    <div className="pr-5 pb-3">
                         <InputField fieldName="Add new product" width={275} type="text" updateForm={updateForm}
                                     propertyNameOfObjToSet="name" required={true}/>
                     </div>
 
-                    {/*<InputAutocompleteField fieldName="category" width={275} data={data} updateForm={updateForm}*/}
-                    {/*                        propertyNameOfObjToSet="categoryId"/>*/}
-
+                    <div className="pr-3 pb-3">
+                        <InputAutocompleteField fieldName="Set category of product" width={275} data={categoriesFromDb}
+                                                updateForm={updateForm}
+                                                propertyNameOfObjToSet="categoryId"/>
+                    </div>
 
                     <div className="pt-3 self-center">
                         <ButtonComponent color="white" bgColor="blue" text="Add" borderRadius="1px"
