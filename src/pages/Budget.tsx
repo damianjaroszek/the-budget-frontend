e03d1680f4074966b6b9df8c2b68024ccec9bca5
DJ <damianjaroszek90@gmail.com>
2023-04-04 19:44:27 +0200

    Added: - budgetInitialValue, - getBudget state - isDataSet state - useEffect hook with fetching data from db with expense and budget - properly displaying form for budget and expense with button update

--- src/pages/Budget.tsx
+++ src/pages/Budget.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import {DiffBar} from "../components/Charts/DiffBar";
 import {Pie} from "../components/Charts/Pie";
+import {Button} from "../components/Button";
+import {fetchData} from "../utils/fetch-data";
+import {constHostAddress} from "../utils/global-const";
+import {BudgetEntity} from 'types';
 
 const legendLabelsForDiffBar = ["Budget", "Expense"];
 const compareTwoValuesForDiffBar = [11300, 3675];
@@ -16,22 +20,112 @@ const dataForPie: [string, number][] = [
     ["Sleep", 30],
 ];
 
+const budgetInitialValue = {
+    budget: 0,
+    expense: 0,
+}
+
 export const Budget = () => {
-    // const foobar: TestInterface = {     TESTOWANIE TYPÓW BE --> FE
-    //     x: 123,
+    const [getBudget, setGetBudget] = useState<BudgetEntity>(budgetInitialValue);
+    const [isDataSet, setIsDataSet] = useState<boolean>(false);
+
+    // const saveShopToDb = async (e: SyntheticEvent) => {
+    //     e.preventDefault();
+    //         const data = await fetchData(constHostAddress, '/product', '', {
+    //             method: 'POST',
+    //             body: JSON.stringify(
+    //                 newProductFromForm
+    //             ),
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //             },
+    //         });
+    //
+    //         setIsDataSet(prevState => !prevState);
+    //
+    //
+    //     setNewProductFromForm(shopInitValues);
     // }
-    return (
-        <div className="mt-24">
-            <div className="flex flex-wrap justify-center">
-                <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
-                    <div className="flex justify-between">
-                        <p className="text-xl font-semibold">Monthly Stats</p>
+
+    useEffect(() => {
+        //setGetBudget(budgetInitialValue);
+        const getBudgetFromDb = async () => {
+            const budget = await fetchData(constHostAddress, '/budget/showBudgetExpense');
+            setGetBudget(budget[0])
+        }
+        getBudgetFromDb().catch(console.error);
+        setIsDataSet(true);
+
+    }, []);//isDataSet
+
+
+    return (<>
+            <div className="mt-24">
+                <div className="flex flex-wrap justify-center">
+                    <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
+                        <div className="flex justify-between">
+                            <p className="text-3xl font-semibold">Budget vs Expense</p>
+                        </div>
+
+                        <div className="flex justify-between pt-10">
+                            <div className="text-xl font-semibold">Budget [PLN]:
+                                <form className="flex justify-start" onSubmit={() => {
+                                }}>
+                                    <input
+                                        style={{width: '250px'}}
+                                        className="pl-2 text-4xl"
+                                        type={"number"}
+                                        min={0}
+                                        maxLength={9}
+                                        size={9}
+                                        value={getBudget.budget.toFixed(2)}
+                                        step={0.1}
+                                        onChange={(e: ChangeEvent<HTMLInputElement>) => setGetBudget(getBudget => ({
+                                            ...getBudget,
+                                            budget: Number(e.target.value),
+                                        }))}
+
+                                    >
+                                    </input>
+                                    <div className="self-center">
+                                        <Button color="white" bgColor="blue" text="Update" borderRadius="1px"
+                                                type="submit" paddingRight={3} paddingLeft={3} paddingBottom={3}
+                                                paddingTop={3}/>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+
+                        <div className="flex justify-between pt-5">
+                            <p className="text-xl font-semibold">Expense [PLN]:
+                                {getBudget && <input
+                                    style={{width: '300px'}}
+                                    className="pl-2 text-4xl"
+                                    type={"number"}
+                                    min={0}
+                                    value={(getBudget.expense).toFixed(2)}
+                                    step={0.1}
+                                    disabled={true}
+                                >
+                                </input>}
+                            </p>
+                        </div>
+
+                        <div className="mt-1 ">
+
+                        </div>
+
                     </div>
 
-                    <div className="mt-1 ">
-                        <DiffBar
-                            title="Budget vs Expense - current month"
-                            widthPercentage={100}
+                    <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
+                        <div className="flex justify-between">
+                            <p className="text-xl font-semibold">Monthly Stats</p>
+                        </div>
+
+                        <div className="mt-1 ">
+                            <DiffBar
+                                title="Budget vs Expense - current month"
+                                widthPercentage={100}
                             heightPixels={250}
                             legendPosition="bottom"
                             chartAreaWidthPercentage={100}
@@ -41,6 +135,7 @@ export const Budget = () => {
                     </div>
 
                 </div>
+
                 <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
                     <div className="flex justify-between">
                         <p className="text-xl font-semibold">Structure of expense</p>
@@ -58,8 +153,11 @@ export const Budget = () => {
                     </div>
 
                 </div>
-            </div>
+
+
+                </div>
 
         </div>
+        </>
     );
 };
